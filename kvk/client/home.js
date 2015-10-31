d922e3481bd123954293171bb47fa30faed92313
Herman Banken <hermanbanken@gmail.com>
2015-10-31 19:54:49 +0100

    Hide compose form

--- kvk/client/home.js
+++ kvk/client/home.js
@@ -2,6 +2,10 @@ Template.Home.helpers({
 	
 	inititiatives: function(){
 		return Initiatives.find({}, { sort: { date: -1 } });
+	},
+	
+	composeOpen: function(){
+		return Session.get("compose-open");
 	}
 	
 })
@@ -21,5 +25,14 @@ Template.Home.events({
 	},
 	"reset form": function(){
 		
+	},
+	"mouseenter .mdi-content-add": function(evt){
+		$(evt.target).removeClass("mdi-content-add").addClass("mdi-content-create");
+	},
+	"mouseleave .mdi-content-create": function(evt){
+		$(evt.target).removeClass("mdi-content-create").addClass("mdi-content-add");
+	},
+	"click .compose > a": function(evt){
+		Session.set("compose-open", !Session.get("compose-open"));
 	}
 })
