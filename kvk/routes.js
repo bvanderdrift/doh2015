c25c6b15bff30d3dd1490b57518ba9436ccbeac7
Bastiaan Grisel <bastiaan.grisel@gmail.com>
2015-10-31 18:25:31 +0100

    Initiative page

--- kvk/routes.js
+++ kvk/routes.js
@@ -5,3 +5,10 @@ Router.configure({
 Router.route("/", function () {
   this.render('Home');
 });
+
+Router.route("/initiative/:id", function () {
+  this.render('Initiative', {
+	data: function () {
+	  return Initiatives.findOne({_id: this.params.id});
+	}});
+});
