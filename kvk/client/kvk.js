9387f08decb058b19f736ab1da9c53d5ca4db5ec
Bastiaan Grisel <bastiaan.grisel@gmail.com>
2015-11-01 01:11:55 +0100

    Search based on KvK Number

--- kvk/client/kvk.js
+++ kvk/client/kvk.js
@@ -2,7 +2,7 @@ Template.ApplicationLayout.events({
     "submit form#search-company-name": function(evt) {
         var keywords = $(evt.target).find("input");
 
-        Meteor.call('search', keywords.val(), function(err, response){
+        Meteor.call('filter', keywords.val(), function(err, response){
 			Session.set('kvkData', response);
 		});
 
