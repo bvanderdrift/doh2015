c03cacb4b8be3959e97a2f668a6f1ac22aa3f165
Bastiaan Grisel <bastiaan.grisel@gmail.com>
2015-11-01 02:44:29 +0100

    Move to new location

--- kvk/client/maps.js
+++ kvk/client/maps.js
@@ -1,11 +1,25 @@
 Template.Home.onRendered(function() {
-	var kvkData = Session.get("kvkData");
-	var myLatlng = new google.maps.LatLng(kvkData.gpsLatitude, kvkData.gpsLongitude);
-	var mapOptions = {
-	  zoom: 13,
-	  center: myLatlng,
-	  mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	var map = new google.maps.Map(document.getElementById("home-map"),
-	    mapOptions);
-});
+
+		// Deps.autorun(function() {
+		// 	var kvkData = Session.get("kvkData");
+		// 	var myLatlng = new google.maps.LatLng(kvkData.gpsLatitude, kvkData.gpsLongitude);
+		// 	var mapOptions = {
+		// 	  zoom: 13,
+		// 	  center: myLatlng,
+		// 	  mapTypeId: google.maps.MapTypeId.ROADMAP
+		// 	};
+		// });
+
+		var mapOptions = {
+		  zoom: 13,
+		  mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+	
+		var map = new google.maps.Map(document.getElementById("home-map"), mapOptions);
+
+		Deps.autorun(function() {
+			var kvkData = Session.get("kvkData");
+			var myLatlng = new google.maps.LatLng(kvkData.gpsLatitude, kvkData.gpsLongitude);
+			map.setCenter(myLatlng);
+		});
+	});
